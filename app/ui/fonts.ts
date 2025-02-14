dd9d860eadd631605feec9eed81ff3553c8a3ffc
lsq4838 <71804111+lsq4838@users.noreply.github.com>
2025-02-14 16:42:11 +0800

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana } from "next/font/google";
+
+// primary font
+export const inter = Inter({ subsets: ["latin"] });
+
+// secondary font
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"]
+})
