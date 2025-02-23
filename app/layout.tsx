4412164b69f193d966fbf545dcb5e276308d2e94
lsq4838 <71804111+lsq4838@users.noreply.github.com>
2025-02-23 20:26:05 +0800

    11

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
     default: "Acme Dashboard",
   },
   description: "The official Next.js Course Dashboard, built with App Router.",
-  metadataBase: new URL("https://nextjs-dashboard-4u45.vercel.app/")
+  // metadataBase: new URL("https://nextjs-dashboard-4u45.vercel.app/")
 }
 
 export default function RootLayout({
